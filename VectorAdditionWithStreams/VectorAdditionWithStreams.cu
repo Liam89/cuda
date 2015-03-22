ac30f1f6a93fcd1f98e0b5bbb0559996e0ff4431
Liam89 <liamallen89@hotmail.com>
2015-03-22 19:58:25 +0000

    [VecAddWithStreams] Complete

--- VectorAdditionWithStreams/VectorAdditionWithStreams.cu
+++ VectorAdditionWithStreams/VectorAdditionWithStreams.cu
@@ -12,6 +12,9 @@
 				        }                                                                     \
 			    } while(0)
 
+// Must be at least 2
+#define NUM_STREAMS 4
+
 __global__ void vecAdd(float *in1, float *in2, float *out, int len) {
 	int i = blockDim.x*blockIdx.x + threadIdx.x;
 	if (i < len) {
@@ -28,13 +31,21 @@ int main(int argc, char **argv) {
 	float *deviceInput1;
 	float *deviceInput2;
 	float *deviceOutput;
+	cudaStream_t streams[NUM_STREAMS];
+
+	for (int i = 0; i < NUM_STREAMS; ++i){
+		cudaStreamCreate(streams + i);
+	}
 
 	args = wbArg_read(argc, argv);
 
 	wbTime_start(Generic, "Importing data and creating memory on host");
 	hostInput1 = (float *)wbImport(wbArg_getInputFile(args, 0), &inputLength);
 	hostInput2 = (float *)wbImport(wbArg_getInputFile(args, 1), &inputLength);
-	hostOutput = (float *)malloc(inputLength * sizeof(float));
+	// Use pinned memory
+	cudaHostRegister(hostInput1, inputLength, 0);
+	cudaHostRegister(hostInput2, inputLength, 0);
+	cudaHostAlloc((void **)&hostOutput, inputLength * sizeof(float),0);
 	wbTime_stop(Generic, "Importing data and creating memory on host");
 
 	wbLog(TRACE, "The input length is ", inputLength);
@@ -45,21 +56,47 @@ int main(int argc, char **argv) {
 	wbCheck(cudaMalloc((void **)&deviceOutput, inputLength*sizeof(float)));
 	wbTime_stop(GPU, "Allocating GPU memory.");
 
-	wbTime_start(GPU, "Copying input memory to the GPU.");
-	wbCheck(cudaMemcpy(deviceInput1, hostInput1, inputLength*sizeof(float), cudaMemcpyHostToDevice));
-	wbCheck(cudaMemcpy(deviceInput2, hostInput2, inputLength*sizeof(float), cudaMemcpyHostToDevice));
-	wbTime_stop(GPU, "Copying input memory to the GPU.");
+	// Vector addition with streams
+	// For each stream, memory will be copied to the device, the kernal launched, and the memory copied from the device
+	// Therefore, the main loop consists of copying memory from the device for stream s-1, launching the kernal for stream s, 
+	//	and copying memory to the device for stream s+1
+	wbTime_start(Compute, "Performing computation");
+	int segSize = (inputLength - 1) / NUM_STREAMS + 1;
+	int offset = 0;
+	int streamNum = 0;
+
+	// Set up loop for stream 0 and stream 1
+	wbCheck(cudaMemcpyAsync(deviceInput1 + offset, hostInput1 + offset, segSize*sizeof(float), cudaMemcpyHostToDevice, streams[streamNum]));
+	wbCheck(cudaMemcpyAsync(deviceInput2 + offset, hostInput2 + offset, segSize*sizeof(float), cudaMemcpyHostToDevice, streams[streamNum]));
 
 	dim3 dimGrid((inputLength - 1) / 256 + 1, 1, 1);
 	dim3 dimBlock(256, 1, 1);
-	wbTime_start(Compute, "Performing CUDA computation");
-	vecAdd << <dimGrid, dimBlock >> >(deviceInput1, deviceInput2, deviceOutput, inputLength);
-	cudaDeviceSynchronize();
-	wbTime_stop(Compute, "Performing CUDA computation");
+	vecAdd<<<dimGrid, dimBlock, 0, streams[streamNum]>>>(deviceInput1 + offset, deviceInput2 + offset, deviceOutput + offset, segSize);
 
-	wbTime_start(Copy, "Copying output memory to the CPU");
-	wbCheck(cudaMemcpy(hostOutput, deviceOutput, inputLength*sizeof(float), cudaMemcpyDeviceToHost));
-	wbTime_stop(Copy, "Copying output memory to the CPU");
+	streamNum = 1;
+	offset += segSize;
+	wbCheck(cudaMemcpyAsync(deviceInput1 + offset, hostInput1 + offset, segSize*sizeof(float), cudaMemcpyHostToDevice, streams[streamNum]));
+	wbCheck(cudaMemcpyAsync(deviceInput2 + offset, hostInput2 + offset, segSize*sizeof(float), cudaMemcpyHostToDevice, streams[streamNum]));
+
+	// Loop through streams
+	for (streamNum = 1; streamNum < NUM_STREAMS - 1; ++streamNum, offset += segSize){
+		wbCheck(cudaMemcpyAsync(deviceInput1 + offset + segSize, hostInput1 + offset + segSize, segSize*sizeof(float), cudaMemcpyHostToDevice, streams[streamNum + 1]));
+		wbCheck(cudaMemcpyAsync(deviceInput2 + offset + segSize, hostInput2 + offset + segSize, segSize*sizeof(float), cudaMemcpyHostToDevice, streams[streamNum + 1]));
+
+		vecAdd<<<dimGrid, dimBlock, 0, streams[streamNum]>>>(deviceInput1 + offset, deviceInput2 + offset, deviceOutput + offset, segSize);
+
+		wbCheck(cudaMemcpyAsync(hostOutput + offset - segSize, deviceOutput + offset - segSize, segSize*sizeof(float), cudaMemcpyDeviceToHost, streams[streamNum - 1]));
+	}
+
+	vecAdd<<<dimGrid, dimBlock, 0, streams[streamNum]>>>(deviceInput1 + offset, deviceInput2 + offset, deviceOutput + offset, segSize);
+	
+	wbCheck(cudaMemcpyAsync(hostOutput + offset - segSize, deviceOutput + offset - segSize, segSize*sizeof(float), cudaMemcpyDeviceToHost, streams[streamNum - 1]));
+
+	wbCheck(cudaMemcpyAsync(hostOutput + offset, deviceOutput + offset, segSize*sizeof(float), cudaMemcpyDeviceToHost, streams[streamNum]));
+
+	cudaDeviceSynchronize();
+	wbTime_stop(Compute, "Performing computation");
+
 
 	wbTime_start(GPU, "Freeing GPU Memory");
 	wbCheck(cudaFree(deviceInput1));
@@ -69,9 +106,11 @@ int main(int argc, char **argv) {
 
 	wbSolution(args, hostOutput, inputLength);
 
+	cudaHostUnregister(hostInput1);
+	cudaHostUnregister(hostInput2);
 	free(hostInput1);
 	free(hostInput2);
-	free(hostOutput);
+	cudaFreeHost(hostOutput);
 
 	return 0;
 }
